1c1cdcf361282d70cdfd5a9c04e895b94703e3ba
jaron <jaron@jaronnie.com>
2025-09-28 13:50:05 +0800

    feat(server): update

--- api/client.go
+++ api/client.go
@@ -4,13 +4,15 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/jzero-io/jzero-admin/server/server/custom"
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/rest"
 
+	"github.com/jzero-io/jzero-admin/server/cmd"
+	"github.com/jzero-io/jzero-admin/server/plugins"
 	"github.com/jzero-io/jzero-admin/server/server/config"
+	"github.com/jzero-io/jzero-admin/server/server/global"
 	"github.com/jzero-io/jzero-admin/server/server/handler"
 	"github.com/jzero-io/jzero-admin/server/server/middleware"
 	"github.com/jzero-io/jzero-admin/server/server/svc"
@@ -27,12 +29,6 @@ type EnvConfigurator struct {
 func (e *EnvConfigurator) GetConfig() (config.Config, error) {
 	logx.Infof("get config from env: key: %s, value: %s", e.Key, os.Getenv(e.Key))
 
-	if os.Getenv(e.Key) == "" {
-		if err := os.Setenv(e.Key, `{"log":{"encoding":"plain"},"rest":{"host":"0.0.0.0","name":"server-api","port":8001,"timeout": 10000}, "sqlx":{"driverName":"pgx","dataSource": "postgres://neondb_owner:npg_le3oEzmNMr9u@ep-nameless-bar-a4xuvs05-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"},"cacheType":"local"}`); err != nil {
-			return config.Config{}, err
-		}
-	}
-
 	var c config.Config
 	if err := conf.LoadFromJsonBytes([]byte(os.Getenv(e.Key)), &c); err == nil {
 		return c, nil
@@ -46,7 +42,12 @@ func (e *EnvConfigurator) AddListener(listener func()) {
 }
 
 func init() {
-	cc := &EnvConfigurator{Key: "CONFIG"}
+	key := "CONFIG"
+	if os.Getenv(key) == "" {
+		_ = os.Setenv(key, `{"log":{"encoding":"plain"},"rest":{"host":"0.0.0.0","name":"server-api","port":8001,"timeout": 10000}, "sqlx":{"driverName":"pgx","dataSource": "postgres://neondb_owner:npg_le3oEzmNMr9u@ep-nameless-bar-a4xuvs05-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"},"cacheType":"local"}`)
+	}
+
+	cc := &EnvConfigurator{Key: key}
 	c, err := cc.GetConfig()
 	logx.Must(err)
 
@@ -56,25 +57,22 @@ func init() {
 		logx.AddWriter(logx.NewWriter(os.Stdout))
 	}
 
-	server := rest.MustNewServer(c.Rest.RestConf, rest.WithCustomCors(func(header http.Header) {
-		header.Set("Access-Control-Allow-Origin", "*")
-		header.Add("Access-Control-Allow-Headers", "X-Request-Id")
-		header.Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-	}, nil, "*"))
-
-	ctm := custom.New(server)
-	ctm.Init()
-
-	middleware.Register(server)
+	s := cmd.NewServer(c.Rest.RestConf)
+	logx.Must(s.Custom.Init(cc))
 
 	svcCtx := svc.NewServiceContext(cc, handler.Route2Code)
-	handler.RegisterHandlers(server, svcCtx)
+	svcCtx.Middleware = middleware.NewMiddleware(svcCtx, handler.Route2Code)
+	global.ServiceContext = *svcCtx
+	middleware.Register(s.Rest)
+	handler.RegisterHandlers(s.Rest, svcCtx)
 
-	Serverless, err = rest.NewServerless(server)
+	plugins.LoadPlugins(s.Rest, *svcCtx)
+
+	Serverless, err = rest.NewServerless(s.Rest)
 	logx.Must(err)
 
 	group := service.NewServiceGroup()
-	group.Add(ctm)
+	group.Add(s.Custom)
 	group.Start()
 }
 
